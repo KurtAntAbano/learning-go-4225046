14306e28973f48b9bae85ac8bdb0b0f929318c8e
KurtAntAbano <116030268+KurtAntAbano@users.noreply.github.com>
2025-11-09 17:38:07 +0000

    using go routines and concurrency

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	go say("HEY goroutine") // go = using go routines 
+	// if u run this u'll notice the bottom line executes first
+	// this is because we added a delay to the say funciton
 	fmt.Println("Goroutines")
+
+	//anomynous function
+
+	go func(message string){
+		fmt.Println(message)
+	} ("Hello from the anoymnous function")
+	
+
+
+	time.Sleep(2*time.Second)
+	fmt.Println("ALL DONE")
+
+}
+
+func say(message string) {
+	time.Sleep(1*time.Second)
+	fmt.Println(message)
 }
