99f098eccdee0e15d36b08ecaa7a80209d132975
KurtAntAbano <116030268+KurtAntAbano@users.noreply.github.com>
2025-11-09 13:02:19 +0000

    using structs

--- Ch03/03_06e/main.go
+++ Ch03/03_06e/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	poodle := Dog{"Poodle", 34}
 	fmt.Println(poodle)
-	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("%+v\n", poodle) //+v means both the names and the values of the fields 
 	fmt.Printf("Breed: %v, Weight: %v\n", poodle.Breed, poodle.Weight)
 	poodle.Weight = 29
 	fmt.Println(poodle)
